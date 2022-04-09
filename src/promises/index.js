88709d324fdf5dcfb84da81810be42b14fa89830
Ascended300 <eduardofelix014@gmail.com>
2022-04-09 14:40:55 -0400

    Doble promesa

--- src/promises/index.js
+++ src/promises/index.js
@@ -12,3 +12,20 @@ const somethingWillHappen = () => {
 somethingWillHappen()
     .then(response => console.log(response))
     .catch(err => console.log(err));
+
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if ( true ) {
+            setTimeout(() => {
+                resolve('True');
+            },2000)
+        } else {
+            const error = new Error('Whoops!');
+            reject(error);
+        }
+    })
+}
+
+somethingWillHappen2()
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
