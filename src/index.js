a2d79701785dc7278ca403a873f79ba19919d529
Ascended300 <eduardofelix014@gmail.com>
2022-04-09 14:11:47 -0400

    Practica 1 de callback

--- src/index.js
+++ src/index.js
@@ -7,3 +7,17 @@ function cal(a, b, callback){
 }
 
 console.log(cal(2,2,sum))
+
+function date(callback) {
+    console.log(new Date);
+    setTimeout(function (){
+        let date = new Date;
+        callback(date);
+    }, 3000)
+}
+
+function printDate(dateNow){
+    console.log(dateNow);
+}
+
+date(printDate);
